41ff021759a66addf176bf7278c7ada3557f40d5
Elepert <emily.lepert@students.olin.edu>
2018-01-27 17:27:25 -0500

    complete first pass at hw

--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -1,42 +1,91 @@
+import java.util.Arrays;
+
 public class MyArrayList {
     private Cow[] elems;
+
     private int size;
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)
     public MyArrayList() {
-        // TODO
+        size = 0;
+        elems = new Cow[10];
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)
     public MyArrayList(int capacity) {
-        // TODO
+        size = 0;
+        elems = new Cow[capacity];
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)*
     public void add(Cow c) {
-        // TODO
+        if (size == elems.length) {
+            grow();
+        }
+        elems[size] = c;
+
+        size++;
+
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)
     public int size() {
-        // TODO
-        return -1;
+        return size;
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(1)
     public Cow get(int index) {
-        // TODO
-        return null;
+        if (index <= size && index >= 0){
+            Cow searchCow = elems[index];
+            return searchCow;
+        } else {
+            throw new IndexOutOfBoundsException("IndexOutOfBoundsException");
+        }
+
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(n)
     public Cow remove(int index) {
-        // TODO
-        return null;
+        if (index <= size && index >= 0) {
+            Cow removedCow = elems[index];
+            for (int i = index; i < size-1; i++) {
+                elems[i] = elems[i + 1];
+            }
+            elems[size-1]= null;
+            size--;
+
+            if (size <= 0.25*elems.length && size != 1){
+                Cow[] tempelems = new Cow[elems.length/2];
+                System.arraycopy(elems, 0, tempelems, 0, elems.length/2);
+                elems = tempelems;
+            }
+            return removedCow;
+        }  else {
+            throw new IndexOutOfBoundsException("Index Out Of Bounds Exception");
+        }
     }
 
-    // TODO: Runtime: O(?)
+    // Runtime: O(n)
     public void add(int index, Cow c) {
-        // TODO
+        if (index <= size && index >= 0) {
+            if (size == elems.length){
+                grow();
+            }
+            for (int i = size-1; i > index; i--) {
+                elems[i] = elems[i - 1];
+            }
+            elems[index] = c;
+
+            size++;
+        }  else {
+            throw new IndexOutOfBoundsException("Index Out Of Bounds Exception");
+        }
+    }
+
+    // Runtime: O(n)
+    public void grow(){
+        Cow[] tempelems = new Cow[elems.length*2];
+        System.arraycopy(elems, 0, tempelems, 0, elems.length);
+        elems = tempelems;
     }
 }
